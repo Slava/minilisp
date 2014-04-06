5c3ba467c05a30b38281ea2731574c8c0e18bdd7
Slava Kim <slava@meteor.com>
2014-04-05 17:32:28 -0700

    add extra header for linux

--- prompt.c
+++ prompt.c
@@ -3,6 +3,11 @@
 
 #include <editline/readline.h>
 
+#ifndef __APPLE__
+// is not required on Mac OS X, is required on Linux
+#include <editline/history.h>
+#endif
+
 int main(int argc, char** argv) {
 
   printf("Minilisp Version 0.0.1\n");
